580b9ea7410cf8defe06181211ec46910b96111f
PhucHoang <phucchu390@gmail.com>
2023-01-17 13:50:59 +0700

    Add Random Function (Can random, not playable yet)

--- src/Level.cpp
+++ src/Level.cpp
@@ -1,21 +1,32 @@
 #include "Level.h"
 #include <ctime>
 
-int random() {
-    srand(time(NULL));
-    return rand() % 33 + 1;                // 1 <= random <= 34
+std::vector<std::vector<int>> random() {            // 0 <= random <= 32
+    srand(time(0));
+    std::vector<std::vector<int>> ans;
+    ans.resize(TILES_HEIGHT);
+    for (int i = 0; i < TILES_HEIGHT; ++i) {
+        ans[i].resize(TILES_WIDTH);
+    }
+    for (int i = 0; i < TILES_HEIGHT; ++i) {
+        for (int j = 0; j < TILES_WIDTH; ++j) {
+            ans[i][j] = rand() % 33;
+        }
+    }
+    return ans;
 }
 
 Level::Level() {
     currentTime = 300;                     // 5 minutes 
     isOver = false;
     tiles.resize(TILES_HEIGHT);
+    std::vector<std::vector<int>> randomMatrix = random();
     for (int i = 0; i < TILES_HEIGHT; ++i) {
         tiles[i].resize(TILES_WIDTH);
     }
     for (int i = 0; i < TILES_HEIGHT; ++i) {
         for (int j = 0; j < TILES_WIDTH; ++j) {
-            tiles[i][j] = new Tiles(random(), j, i);
+            tiles[i][j] = new Tiles(randomMatrix[i][j], j, i);
         }
     }
     totalTiles = TILES_HEIGHT * TILES_WIDTH;
